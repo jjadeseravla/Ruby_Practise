ff4214914e79a54dc98f465ebb31d87ab9c38f20
jade <jadealvares@hotmail.co.uk>
2018-11-26 19:16:42 +0000

    refactored test to start with new instance (mr) of object...but code all broke (WIP)

--- lib/marsrover.rb
+++ lib/marsrover.rb
@@ -1,6 +1,7 @@
 class MarsRover
   DEFAULT_POSITION = [0,0]
   DEFAULT_DIRECTION = :north
+  COMPASS_DIRECTIONS = [:north, :west, :south, :east]
 
   def initialize
     @position = DEFAULT_POSITION
@@ -12,12 +13,21 @@ class MarsRover
   end
 
   def direction
-    @direction
+    COMPASS_DIRECTIONS[@direction]
   end
 
-  def receive_commands(commands)
+  def receive_commands(*commands)
     @position = [0,1]
-    @position = [1,0] if commands == :b
+    @position = [0,-1] if commands.first == :b
+    commands.each do |theCommand|
+      turn_left if theCommand == :l
+    end
+  end
+
+  private
+
+  def turn_left
+    @direction += 1 % (COMPASS_DIRECTIONS.size)
   end
 
 end
--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -5,29 +5,38 @@ describe 'marsrover' do
   ORIGIN = [0,0]
   DIRECTION = :north
 
+  let(:mr) { MarsRover.new }
 
   it 'starts at original position' do
-    mr = MarsRover.new
     expect(mr.position).to eq(ORIGIN)
   end
 
   it 'can move forward' do
     ONE_STEP_NORTH = [0,1]
-    mr = MarsRover.new
-    mr.receive_commands(:f)
+    mr.receive_commands([:f])
     expect(mr.position).to eq(ONE_STEP_NORTH)
   end
 
   it 'can move backwards' do
-    ONE_STEP_BACKWARD = [1,0]
-    mr = MarsRover.new
-    mr.receive_commands(:b)
-    expect(mr.position).to eq(ONE_STEP_BACKWARD)
+    ONE_STEP_SOUTH = [0,-1]
+    mr.receive_commands([:b])
+    expect(mr.position).to eq(ONE_STEP_SOUTH)
   end
 
-  it "knows which direction it is facing" do
-    mr = MarsRover.new
+  it "knows which direction it is facing at the beginning, which is north" do
     expect(mr.direction).to eq(DIRECTION)
+  end
+
+  it 'can turn left' do
+    mr.receive_commands([:l])
+    expect(mr.direction).to eq(:west)
+  end
+
+  it 'can change directions' do
+    mr.receive_commands([:l, :l])
+    expect(mr.direction).to eq(:south)
+    mr.receive_commands([:l, :l])
+    expect(mr.direction).to eq(:north)
   end
 
 end
