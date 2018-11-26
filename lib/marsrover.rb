96d9658ea6ffecfd8de27f3e33e3f507245a19e6
jade <jadealvares@hotmail.co.uk>
2018-11-26 15:15:16 +0000

    can move foward

--- lib/marsrover.rb
+++ lib/marsrover.rb
@@ -1,7 +1,15 @@
 class MarsRover
 
+  def initialize
+    @position = [0,0]
+  end
+
   def position
-    [0,0]
+    @position
+  end
+
+  def receive_commands(commands)
+    @position = [0,1]
   end
 
 end
--- spec/marsrover_spec.rb
+++ spec/marsrover_spec.rb
@@ -5,4 +5,10 @@ describe 'marsrover' do
     mr = MarsRover.new
     expect(mr.position).to eq([0,0])
   end
+
+  it 'can move forward' do
+    mr = MarsRover.new
+    mr.receive_commands('f')
+    expect(mr.position).to eq([0,1])
+  end
 end
